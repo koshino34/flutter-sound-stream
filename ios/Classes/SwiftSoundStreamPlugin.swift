19c9345a92c158b2295ad7dd536737b344328a93
koshino <74943318+koshino34@users.noreply.github.com>
2024-01-04 10:02:42 +0900

    Update SwiftSoundStreamPlugin.swift

--- ios/Classes/SwiftSoundStreamPlugin.swift
+++ ios/Classes/SwiftSoundStreamPlugin.swift
@@ -59,6 +59,7 @@ public class SwiftSoundStreamPlugin: NSObject, FlutterPlugin {
         super.init()
         self.attachPlayer()
         mAudioEngine.prepare()
+        let audioSession = AVAudioSession.sharedInstance() do { try audioSession.overrideOutputAudioPort(.speaker) } catch { print("error overriding output audio port") }
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
