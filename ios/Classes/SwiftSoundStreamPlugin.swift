db99d33d652ec13c448535494f670fef7918688d
koshino <74943318+koshino34@users.noreply.github.com>
2024-02-04 20:06:44 +0900

    Update SwiftSoundStreamPlugin.swift

--- ios/Classes/SwiftSoundStreamPlugin.swift
+++ ios/Classes/SwiftSoundStreamPlugin.swift
@@ -29,7 +29,7 @@ public class SwiftSoundStreamPlugin: NSObject, FlutterPlugin {
     private let mRecordBus = 0
     private var mInputNode: AVAudioInputNode
     private var mRecordSampleRate: Double = 16000 // 16Khz
-    private var mRecordBufferSize: AVAudioFrameCount = 6400
+    private var mRecordBufferSize: AVAudioFrameCount = 2730
     private var mRecordChannel = 0
     private var mRecordSettings: [String:Int]!
     private var mRecordFormat: AVAudioFormat!
@@ -39,7 +39,7 @@ public class SwiftSoundStreamPlugin: NSObject, FlutterPlugin {
     private let mPlayerBus = 0
     private let mPlayerNode = AVAudioPlayerNode()
     private var mPlayerSampleRate: Double = 16000 // 16Khz
-    private var mPlayerBufferSize: AVAudioFrameCount = 6400
+    private var mPlayerBufferSize: AVAudioFrameCount = 2730
     private var mPlayerOutputFormat: AVAudioFormat!
     private var mPlayerInputFormat: AVAudioFormat!
     
